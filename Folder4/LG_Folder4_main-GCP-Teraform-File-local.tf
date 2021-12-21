4f89856c782cdf88b054049ac0c5445fb4b32d73
deepakmycloud <75788265+deepakmycloud@users.noreply.github.com>
2021-12-21 13:11:00 +0530

    Create LG_Folder4_main-GCP-Teraform-File-local.tf

---
+++ Folder4/LG_Folder4_main-GCP-Teraform-File-local.tf
@@ -0,0 +1,42 @@
+terraform { 
+			backend "local" {} 
+			}
+
+locals {
+  standard_tags = {
+    Component   = "user-service"
+    Environment = "production"
+  }
+}
+
+provider "google" {
+alias = "uscentral1"
+project = var.gcp_projectid
+credentials = file("${var.path}")
+region = "us-central1"
+}
+
+variable "gcp_projectid" {
+default = ""
+}
+variable "path" {
+default = ""
+}
+
+resource "google_compute_instance" "instance1" {
+name = "instance1"
+provider = google.uscentral1
+machine_type = "e2-medium"
+tags = var.mytag
+zone = "us-central1-a"
+metadata = var.md
+boot_disk {
+auto_delete = "true"
+initialize_params {
+image = "debian-cloud/debian-9"
+}
+}
+network_interface {
+network = "default"
+access_config {
+}
