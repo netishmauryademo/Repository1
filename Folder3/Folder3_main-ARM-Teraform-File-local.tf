68e4ccb6d9eb04dd1f8578a65878964b1adfd8ac
deepakmycloud <75788265+deepakmycloud@users.noreply.github.com>
2021-12-22 11:24:52 +0530

    Update Folder3_main-ARM-Teraform-File-local.tf

--- Folder3/Folder3_main-ARM-Teraform-File-local.tf
+++ Folder3/Folder3_main-ARM-Teraform-File-local.tf
@@ -1,10 +1,7 @@
 
-terraform {
-	
-backend "local" {} 
-	
+terraform {	
+backend "local" {} 	
 required_version =">=0.12"
-
 required_providers {
 azurerm = {
 source = "hashicorp/azurerm"
@@ -12,36 +9,29 @@ version ="=2.79.0"
 }
 }
 }
-
-
 locals {
   standard_tags = {
     Component   = "user-service"
     Environment = "production"
   }
 }
-
 provider "arm" {
 client_secret = var.clientsecretkey
 subscription_id = var.azuresubscriptionid
 tenant_id = var.tenantid
-client_id = var.nativclientid
-	
+client_id = var.nativclientid	
 features {
       }
 }
-
 variable "clientsecretkey" {
 }
 variable "azuresubscriptionid" {
 }
-variable "tenantid" {
-	
-}variable "nativclientid" {
+variable "tenantid" {	
 }
-
+variable "nativclientid" {
+}
 resource "azurerm_resource_group" "ResourceGroup1" {
 name = "ResourceGroup1_LandingZone"
 location = "eastus"
-
 }
