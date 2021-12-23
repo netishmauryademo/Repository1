fdaea42f27d906bd54e7403bfcb173e60b4afe0c
deepakmycloud <75788265+deepakmycloud@users.noreply.github.com>
2021-12-23 15:11:59 +0530

    Create ARM-local-Terraform-File.tf

---
+++ Folder6/ARM-local-Terraform-File.tf
@@ -0,0 +1,36 @@
+terraform {	
+backend "local" {} 	
+required_version =">=0.12"
+required_providers {
+azurerm = {
+source = "hashicorp/azurerm"
+version ="=2.79.0"
+}
+}
+}
+locals {
+  standard_tags = {
+    Component   = "user-service"
+    Environment = "production"
+  }
+}
+provider "azurerm" {
+client_secret = var.clientsecretkey
+subscription_id = var.azuresubscriptionid
+tenant_id = var.tenantid
+client_id = var.nativclientid	
+features {
+      }
+}
+variable "clientsecretkey" {
+}
+variable "azuresubscriptionid" {
+}
+variable "tenantid" {	
+}
+variable "nativclientid" {
+}
+resource "azurerm_resource_group" "ResourceGroup1" {
+name = "ResourceGroup1_LandingZone"
+location = "eastus"
+}
