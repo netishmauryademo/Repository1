9600c1e2cedd768661d796b00a041420fac44d25
deepakmycloud <75788265+deepakmycloud@users.noreply.github.com>
2022-01-10 17:14:25 +0530

    Update GCP-Terraform-File.tf

--- Folder5/subfolder/GCP-Terraform-File.tf
+++ Folder5/subfolder/GCP-Terraform-File.tf
@@ -28,7 +28,7 @@ resource "google_compute_instance" "instance1" {
 name = "instance1"
 provider = google.uscentral1
 machine_type = "e2-medium"
-zone = "us-central1-a"
+zone = "us-central1-c"
 boot_disk {
 auto_delete = "true"
 initialize_params {
